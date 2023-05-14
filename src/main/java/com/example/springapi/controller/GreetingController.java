314ef79050bf09f1d66d78e57362558abed116e7
Vini Bjazevich <vinibjazevich@MacBook-Pro.local>
2023-05-14 18:18:18 -0500

    chore: lint fixes

--- src/main/java/com/example/springapi/controller/GreetingController.java
+++ src/main/java/com/example/springapi/controller/GreetingController.java
@@ -1,12 +1,11 @@
 package com.example.springapi.controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import com.example.springapi.dto.MessageDTO;
-
 
 @RestController
 public class GreetingController {
